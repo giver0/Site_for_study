2ce11b54619014dbef5e73b48a751ac0bd64b8f5
giver0 <giver313@gmail.com>
2022-02-08 19:06:28 +0200

    added function check_if_field_not_empty

--- DATRICS/page-sign-up/JavaScript.js
+++ DATRICS/page-sign-up/JavaScript.js
@@ -9,72 +9,20 @@ const button = document.getElementById('button-sign-up')
 const activeButtonColor = '#CC3D39'
 const disabledButtonColor = 'black'
 
-button.style.background = disabledButtonColor
-
-inputFirstName.addEventListener('input', function(event) {
-    if (event.target.value === '') {
-        button.style.background = disabledButtonColor
-        button.onclick = function () {
-            console.log('НЕОБХІДНО ВВЕСТИ ІМЯ')
-        }
-    } else {
-        button.style.background = activeButtonColor
+function check_if_field_not_empty(field_to_check){
+    if(field_to_check.value!=''){
+        return true
     }
-    console.log(event.target.value)
-})
-
-inputLastName.addEventListener('input', function(event) {
-    if (event.target.value === '') {
-        button.style.background = disabledButtonColor
-        button.onclick = function () {
-            console.log('НЕОБХІДНО ВВЕСТИ ПРІЗВИЩЕ')
-        }
-    } else {
-        button.style.background = activeButtonColor
+    else{
+        return false
     }
-    console.log(event.target.value)
-})
+}
 
-inputEmail.addEventListener('input', function(event) {
-    if (event.target.value === '') {
-        button.style.background = disabledButtonColor
-        button.onclick = function () {
-            console.log('НЕОБХІДНО ВВЕСТИ EMAIL')
-        }
-    } else {
-        button.style.background = activeButtonColor
+inputFirstName.addEventListener('input', function(event){
+    console.log(check_if_field_not_empty(event.target.value))
+    if(check_if_field_not_empty(event.target.value)){
+
+        console.log(event.target.value)
     }
-    console.log(event.target.value)
+    
 })
-
-inputPassword.addEventListener('input', function(event) {
-    if (event.target.value === '' && inputPassword != inputConfirmPassword) {
-        button.style.background = disabledButtonColor
-        button.onclick = function () {
-            console.log('НЕОБХІДНО ВВЕСТИ PASSWORD')
-        }
-    } else {
-        button.style.background = activeButtonColor
-    }
-    console.log(event.target.value)
-})
-
-inputConfirmPassword.addEventListener('input', function(event) {
-    if (event.target.value === '') {
-        button.style.background = disabledButtonColor
-        button.onclick = function () {
-            console.log('НЕОБХІДНО ВВЕСТИ ПІДТВЕРДЖЕННЯ PASSWORD')
-        }
-    } else {
-        button.style.background = activeButtonColor
-    }
-    console.log(event.target.value)
-})
-
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
