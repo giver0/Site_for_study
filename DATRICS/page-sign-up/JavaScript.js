fa83949a21eac902fb554c92c9e702414f439218
giver0 <giver313@gmail.com>
2022-02-10 21:11:14 +0200

    added validateFieldNotEmpty2 - but doesn't work

--- DATRICS/page-sign-up/JavaScript.js
+++ DATRICS/page-sign-up/JavaScript.js
@@ -1,21 +1,37 @@
 
+const button = document.getElementById('button-sign-up')
+
 const inputFirstName = document.getElementById('input-first-name')
 const inputLastName = document.getElementById('input-last-name')
 const inputEmail = document.getElementById('input-email')
 const inputPassword = document.getElementById('input-password')
 const inputConfirmPassword = document.getElementById('input-confirm-password')
-const button = document.getElementById('button-sign-up')
+
+const inputFirstNameError = document.getElementById('input-first-name-error')
+const inputLastNameError = document.getElementById('input-last-name-error')
+const inputEmailError = document.getElementById('input-email-error')
+const inputPasswordError = document.getElementById('input-password-error')
+const inputConfirmPasswordError = document.getElementById('input-confirm-password-error')
 
 const activeButtonColor = '#CC3D39'
 const disabledButtonColor = 'gray'
 
 button.style.background = disabledButtonColor
 
+validateFieldNotEmpty2(inputFirstName, "Имя")
 
+function validateFieldNotEmpty2(document, errorName){
+    document.addEventListener('input', function(event){  
+        validateFieldNotEmpty(event.target.value, errorName)    
+    })
+    document.innerText = event.target.value
+
+ }
 
 
 inputFirstName.addEventListener('input', function(event){
        validateFieldNotEmpty(event.target.value, "Имя")
+       inputFirstNameError.innerText = event.target.value
     })
 
 inputLastName.addEventListener('input', function(event){  
